ad5f8ccc0515d21c3d03d14e7ff5bf79e44e183b
TomaNovickaya <126395023+TomaNovickaya@users.noreply.github.com>
2023-07-29 21:06:43 +0300

    Create test2.java

---
+++ HW_2_OOP/Задача1/test2.java
@@ -0,0 +1,31 @@
+package HW_2;
+
+public class test2 implements test1{
+    int age;
+   
+
+    public test2(){
+      age = 27;
+       
+    }
+    public int getNext(){
+     age = 27;
+
+       return age;
+     
+    }
+
+    public void reset(){
+
+     age = 10;
+
+    }
+
+    public void setNum(int x){
+   
+       age = x;
+       
+    }
+
+    
+}
