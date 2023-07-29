986f4184332fac46ab2166cc518a5f5b13ceb6eb
TomaNovickaya <126395023+TomaNovickaya@users.noreply.github.com>
2023-07-29 21:06:26 +0300

    Create test1.java

---
+++ HW_2_OOP/Задача1/test1.java
@@ -0,0 +1,7 @@
+package HW_2;
+
+public interface test1 {
+    int getNext(); //возвратить следующее попорядку число
+    void reset(); //сброс
+    void setNum(int x); // установка начального значения 
+}
