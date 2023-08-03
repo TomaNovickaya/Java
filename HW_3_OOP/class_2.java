dbe0b0b26a1b7485ee295cace718b26326ae6228
TomaNovickaya <126395023+TomaNovickaya@users.noreply.github.com>
2023-08-04 01:10:40 +0300

    Create class_2.java

---
+++ HW_3_OOP/class_2.java
@@ -0,0 +1,17 @@
+package HW_3;
+
+public class class_2 implements MyIf{
+    public int getCat(){
+        return 1996;
+    }
+
+    public int getOwner(){
+    return 1968;
+}
+
+    @Override
+    public int getUserID() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getUserID'");
+    }
+}
