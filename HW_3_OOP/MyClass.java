cd3c429317fd1732f83f022dee6e1ad280376ba7
TomaNovickaya <126395023+TomaNovickaya@users.noreply.github.com>
2023-08-04 01:12:17 +0300

    Create MyClass.java

---
+++ HW_3_OOP/MyClass.java
@@ -0,0 +1,18 @@
+package HW_3;
+
+public class MyClass implements Method2{
+    @Override
+    public void met1() {
+        System.out.println("Кошка - Тамара");
+    }
+
+    @Override
+    public void met2() {
+       System.out.println("Возраст - 27");
+    }
+
+    @Override
+    public void met3() {
+         System.out.println("Владелец - Людмила");
+    }
+}
